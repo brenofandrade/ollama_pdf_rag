df5e5d5401c39693780608d59ae909e12f7eef6b
Breno Andrade <andradefbreno@gmail.com>
2025-07-08 11:53:12 -0300

    update

--- src/main.py
+++ src/main.py
@@ -10,7 +10,41 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-from data_embedding import vectorstore
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+
+
+PATH_DOCUMENTS = 'data/avaliacao_RAG.pdf'
+DB_DIR = 'db/chromadb/'
+
+
+chunk_size = 100
+chunk_overlap = 50
+
+loader = PyPDFLoader(PATH_DOCUMENTS)
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap,
+    # separator=r"\n|\s+",
+    # is_separator_regex=True
+)
+
+docs = text_splitter.split_documents(documents=documents)
+
+MODEL_EMBEDDING = "mxbai-embed-large"
+
+embeddings = OllamaEmbeddings(model=MODEL_EMBEDDING)
+
+vectorstore = Chroma.from_documents(
+    documents=docs,
+    embedding=embeddings,
+    persist_directory=DB_DIR,
+    collection_name='documents'
+)
 
 warnings.filterwarnings('ignore', category=UserWarning)
 
